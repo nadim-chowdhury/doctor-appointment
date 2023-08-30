7f1d190edbd061c945904a359aee3c974256df4b
Nadim Chowdhury <nadim-chowdhury@outlook.com>
2023-08-30 17:59:53 +0600

    rtk implemented

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -4,7 +4,6 @@ const bcrypt = require("bcryptjs");
 // sign up controller
 const signupController = async (req, res) => {
   try {
-    
   } catch (error) {
     console.log(error);
     res.status(500).send({ success: false, message: error.message });
